f132ff64c33af506887b8d3db7312197fd488379
Odhran <mahonyodhran@gmail.com>
2022-07-11 18:31:41 +0100

    added directories / added basic check to see if entries can be found

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 
 sourceDir = "C:/Users/odhra/Downloads"
+documentDir = "C:/Users/odhra/Documents"
+musicDir = "C:/Users/odhra/Music"
+imageDir = "C:/Users/odhra/Pictures"
+videoDir = "C:/Users/odhra/Videos"
 
 with os.scandir(sourceDir) as entries:
     for entry in entries:
-        print(entry)
+        if("mp3" in entry.name):
+            print(entry.name)
