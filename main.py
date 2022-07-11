d0271ecff474acebc3aed19e7033b8b0fcca9754
Odhran <mahonyodhran@gmail.com>
2022-07-11 18:37:20 +0100

    added move method from shutil to move file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
+import shutil
 
-sourceDir = "C:/Users/odhra/Downloads"
-documentDir = "C:/Users/odhra/Documents"
-musicDir = "C:/Users/odhra/Music"
-imageDir = "C:/Users/odhra/Pictures"
-videoDir = "C:/Users/odhra/Videos"
+sourceDir = "C:/Users/odhra/Downloads/"
+documentDir = "C:/Users/odhra/Documents/"
+musicDir = "C:/Users/odhra/Music/"
+imageDir = "C:/Users/odhra/Pictures/"
+videoDir = "C:/Users/odhra/Videos/"
 
 with os.scandir(sourceDir) as entries:
     for entry in entries:
         if("mp3" in entry.name):
-            print(entry.name)
+            shutil.move(sourceDir+entry.name, musicDir)
