77608119f3ee7ccfcc3ef4082294b46972312139
Odhran <mahonyodhran@gmail.com>
2022-10-05 23:40:38 +0100

    removed variable declarations etc - changed var names to pythonic changes - added sql file check

--- main.py
+++ main.py
@@ -1,83 +1,80 @@
 import os
 import shutil
+from directories import book_dir, document_dir, image_dir, music_dir, pdf_dir, source_dir, sql_dir, video_dir
+from extensions import book_extensions, document_extensions, image_extensions, music_extensions, video_extensions
 
-sourceDir = "C:/Users/odhra/Downloads/"
-bookDir = "C:/Users/odhra/Documents/Books"
-documentDir = "C:/Users/odhra/Documents/"
-musicDir = "C:/Users/odhra/Music/"
-pdfDir = "C:/Users/odhra/Documents/PDFs"
-imageDir = "C:/Users/odhra/Pictures/"
-videoDir = "C:/Users/odhra/Videos/"
-
-bookExtensions = [".epub", ".fb2", ".mobi"]
-
-documentExtensions = [".doc", ".docx", ".odt", ".xls", ".xlsx", ".ppt", ".pptx"]
-
-imageExtensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
-                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
-
-musicExtensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
-
-videoExtensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
-                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
 
 def checkBook(name):
-    for bookExtension in bookExtensions:
+    for bookExtension in book_extensions:
         if name.endswith(bookExtension) or name.endswith(bookExtension.upper()):
-            if(fileExists(videoDir, name)):
+            if(fileExists(book_dir, name)):
                 print("File already exists - Skipping . .")
             else:
-                shutil.move(sourceDir+name, bookDir)
-                print(f"Moved book file: {name} to {bookDir}")
+                shutil.move(source_dir+name, book_dir)
+                print(f"Moved book file: {name} to {book_dir}")
+
 
 def checkDocument(name):
-    for documentExtension in documentExtensions:
+    for documentExtension in document_extensions:
         if name.endswith(documentExtension) or name.endswith(documentExtension.upper()):
-            if(fileExists(documentDir, name)):
+            if(fileExists(document_dir, name)):
                 print("File already exists - Skipping . .")
             else:         
-                shutil.move(sourceDir+name, documentDir)
-                print(f"Moved document file: {name} to {documentDir}")
-            
+                shutil.move(source_dir+name, document_dir)
+                print(f"Moved document file: {name} to {document_dir}")
+
+
 def checkImage(name):
-    for imageExtension in imageExtensions:
+    for imageExtension in image_extensions:
         if name.endswith(imageExtension) or name.endswith(imageExtension.upper()):
-            if(fileExists(imageDir, name)):
+            if(fileExists(image_dir, name)):
                 print("File already exists - Skipping . .")
             else:
-                shutil.move(sourceDir+name, imageDir)
-                print(f"Moved image file: {name} to {imageDir}")
-            
+                shutil.move(source_dir+name, image_dir)
+                print(f"Moved image file: {name} to {image_dir}")
+
+
 def checkMusic(name):
-    for musicExtension in musicExtensions:
+    for musicExtension in music_extensions:
         if name.endswith(musicExtension) or name.endswith(musicExtension.upper()):
-            if(fileExists(musicDir, name)):
+            if(fileExists(music_dir, name)):
                 print("File already exists - Skipping . .")
             else:
-                shutil.move(sourceDir+name, musicDir)
-                print(f"Moved music file: {name} to {musicDir}")
-            
+                shutil.move(source_dir+name, music_dir)
+                print(f"Moved music file: {name} to {music_dir}")
+
+
 def checkPDF(name):
     if name.endswith(".pdf") or name.endswith(".pdf".upper()):
-        if(fileExists(pdfDir, name)):
+        if(fileExists(pdf_dir, name)):
             print("File already exists - Skipping . .")
         else:
-            shutil.move(sourceDir+name, pdfDir)
-            print(f"Moved PDF file: {name} to {pdfDir}")
-            
+            shutil.move(source_dir+name, pdf_dir)
+            print(f"Moved PDF file: {name} to {pdf_dir}")
+
+
 def checkVideo(name):
-    for videoExtension in videoExtensions:
+    for videoExtension in video_extensions:
         if name.endswith(videoExtension) or name.endswith(videoExtension.upper()):
-            if(fileExists(videoDir, name)):
+            if(fileExists(video_dir, name)):
                 print("File already exists - Skipping . .")
             else:
-                shutil.move(sourceDir+name, videoDir)
-                print(f"Moved video file: {name} to {videoDir}")
+                shutil.move(source_dir+name, video_dir)
+                print(f"Moved video file: {name} to {video_dir}")
+
+
+def checkSQL(name):
+    if name.endswith('sql'):
+        if fileExists(sql_dir, name):
+            print(f"File {name} exists - Skipping . .")
+        else:
+            shutil.move(source_dir+name, sql_dir)
+            print(f"Moved video file: {name} to {sql_dir}")
 
 def fileExists(directory ,name):
     return os.path.exists(directory + '/' + name)
 
-with os.scandir(sourceDir) as entries:
+with os.scandir(source_dir) as entries:
     for entry in entries:
         name = entry.name
         checkBook(name)
