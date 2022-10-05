931bce464473792ba75c91d02ebbf3cd13c38f50
Odhran <mahonyodhran@gmail.com>
2022-10-05 23:39:43 +0100

    added module for file extensions - could be a dict

---
+++ extensions.py
@@ -0,0 +1,11 @@
+book_extensions = [".epub", ".fb2", ".mobi"]
+
+document_extensions = [".doc", ".docx", ".odt", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+music_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
