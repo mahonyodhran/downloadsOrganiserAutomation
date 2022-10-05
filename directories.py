10b29060990caeab8bf65a3cca65b72321e06757
Odhran <mahonyodhran@gmail.com>
2022-10-05 23:39:20 +0100

    added a module solely for directories

---
+++ directories.py
@@ -0,0 +1,8 @@
+source_dir = "C:/Users/odhra/Downloads/"
+book_dir = "C:/Users/odhra/Documents/Books/"
+document_dir = "C:/Users/odhra/Documents/"
+music_dir = "C:/Users/odhra/Music/"
+pdf_dir = "C:/Users/odhra/Documents/PDFs/"
+sql_dir = "C:/Users/odhra/Documents/SQL_Scripts/"
+image_dir = "C:/Users/odhra/Pictures/"
+video_dir = "C:/Users/odhra/Videos/"
